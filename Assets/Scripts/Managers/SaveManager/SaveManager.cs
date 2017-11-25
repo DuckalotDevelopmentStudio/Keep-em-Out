805abe3a2855683e3bd84fb86e6df365bef509a2
flow____ <32310300+LukasFratzl@users.noreply.github.com>
2017-11-25 19:16:01 +0100

    Created a script

---
+++ Assets/Scripts/Managers/SaveManager/SaveManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Project.Managers {
+	public class SaveManager : MonoBehaviour {
+		
+
+		#region Singleton from SaveManager
+		public static SaveManager m_Instance;
+		#endregion
+
+		void Awake () {
+			#region Singleton from SaveManager
+			if (m_Instance != null) {
+				Debug.LogError("More then 1 instance found " + m_Instance.name);
+				return;
+			}
+			m_Instance = this;
+			#endregion
+		}
+
+		
+		void Start () {
+			
+		}
+		
+	
+		void Update () {
+			
+		}
+	}
+}
