dcbaee77b91fa68212128e2142729bfec32d9dec
novacti3 <32226878+novacti3@users.noreply.github.com>
2018-02-01 21:15:48 +0100

    Started work on GameMaster and Character
    - Removed IDamageable
    - Started work on GameMaster.cs
    - Started work on Character base class (namespace Project.Base)

---
+++ Assets/Scripts/Base Classes/Character.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Project.BaseClases
+{
+    public class Character : MonoBehaviour
+    {        
+        protected void CharacterUpdateCalls()                   /// <summary>Contains all the base class' UpdateCalls (call inside of Update() of a derived class)</summary>
+        {
+            //-- Destroy gameObject if currentHP <= 0
+        }
+
+        public virtual void TakeDamage(int damage)              /// <summary>Subtracts damage from the character's current HP</summary> <param name="damage">Desired amount of damage to subtract</param>
+        {
+            //-- Subtract damage from current HP
+        }
+
+        public virtual void Die()                               /// <summary>Destroys the character</summary>
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/GameMaster/GameMaster.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public enum GameState
+{
+    InWave,
+    AfterWave,
+    Paused
+}
+
+namespace Project.Managers
+{
+    
+    public class GameMaster : MonoBehaviour                     /// <summary>Takes care of managing what's going on in the game</summary>
+    {
+        public GameState gameState = GameState.InWave;          /// <summary>Current game's state</summary>
+
+        public void SwitchGameState(GameState desiredState)     /// <summary>Switches the current game state</summary> /// <param name="desiredState">The desired state to switch to</param>
+        {
+            gameState = desiredState;
+        }
+    }
+}
