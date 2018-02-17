b68a8c76fc3d041b017302020c6cd4b2c68d7733
novacti3 <honsal3@seznam.cz>
2018-02-17 17:32:46 +0100

    Made some fixes to Enemy.cs
    - Edited Enemy.cs so it doesn't register an OnEnemyDie event to the EventManager

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -4,13 +4,6 @@ using Project.Managers;
 
 public class Enemy : Character {
 
-    public static DuckalotEvent EnemyDied;                                  //reference to the EventManager
-
-    private void Start()
-    {
-        EnemyDied = EventManager.RegisterEvent("OnEnemyDied", this);        //registers the event in the EventManager
-    }
-
     private void Update()
     {
         CharacterUpdateCalls();                                             //updates the enemy by using the base character class 
@@ -23,11 +16,5 @@ public class Enemy : Character {
     public override void Die()
     {
         base.Die();                                                         //dies by using the base character class
-        if(EnemyDied != null)                                               
-        {
-            EnemyDied.Invoke();                                             //invokes the EnemyDied event
-        }
-        Debug.Log("Enemy Died");
-    }
-    
+    }    
 }
