4e43a9154864b9d9a30a39e7cffee004ba3ae9f5
Stef Halmans <stefhalmans@ziggo.nl>
2018-02-17 17:10:23 +0100

    Added enemy scrpit
    added the enemy script

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Project.BaseClasses;
+using Project.Managers;
+
+public class Enemy : Character {
+
+    public static DuckalotEvent EnemyDied;                                  //reference to the EventManager
+
+    private void Start()
+    {
+        EnemyDied = EventManager.RegisterEvent("OnEnemyDied", this);        //registers the event in the EventManager
+    }
+
+    private void Update()
+    {
+        CharacterUpdateCalls();                                             //updates the enemy by using the base character class 
+    }
+    public override void TakeDamage(int damage)
+    {
+        base.TakeDamage(damage);                                            //takes damage by using the base character class
+    }
+
+    public override void Die()
+    {
+        base.Die();                                                         //dies by using the base character class
+        if(EnemyDied != null)                                               
+        {
+            EnemyDied.Invoke();                                             //invokes the EnemyDied event
+        }
+        Debug.Log("Enemy Died");
+    }
+    
+}
